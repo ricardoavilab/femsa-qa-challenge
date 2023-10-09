481fe9500d08122a8b0f46464f38cc0ebb1050fd
Ricardo Avila <ricardo.avila@digitalfemsa.com>
2023-10-09 17:55:49 -0600

    add api basic test

--- dat-provider.ts
+++ dat-provider.ts
@@ -1,3 +1,4 @@
+export const URL_MELI="https://www.mercadolibre.com.ar/menu"; 
 export const URL_SAUCEDEMO='https://www.saucedemo.com/'
 export const USERNAME='standard_user'
 export const PASSWORD='secret_sauce'
--- tests/api/departments.test.ts
+++ tests/api/departments.test.ts
@@ -1,5 +1,11 @@
 import { expect, test } from '@playwright/test';
+import { URL_MELI } from '../../dat-provider'
 
-test('get departments @api', async ({ request }) => {
-  expect(request).toBeDefined();
+test('get departments - basic test @api', async ({ request }) => {
+    const response = await request.get(`${URL_MELI}/departments`);
+    const data = await response.json();
+
+    expect(response.ok()).toBeTruthy();
+    expect(data).toHaveProperty('departments');
+    expect(data).toHaveProperty('landings');
 });
